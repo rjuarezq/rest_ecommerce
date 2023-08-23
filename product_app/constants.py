1ddf39be2c49f0da67cb47ba11ab997ebab4ba33
Ricardo Juarez <juarezqricardo@gmail.com>
2023-08-23 18:52:52 -0500

    Add constants for selection of choices in category

---
+++ product_app/constants.py
@@ -0,0 +1,10 @@
+CATEGORY_VIP = "VIP"
+CATEGORY_STANDARD = "Standard"
+CATEGORY_BASIC = "Basic"
+CATEGORY_FREE = "Free"
+_CLASS_CATEGORY = (
+    (CATEGORY_VIP, "VIP"),
+    (CATEGORY_STANDARD, "Standard"),
+    (CATEGORY_BASIC, "Basic"),
+    (CATEGORY_FREE, "Free"),
+)
